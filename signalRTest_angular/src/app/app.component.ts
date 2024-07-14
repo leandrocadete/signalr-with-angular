417272ae4873c605682f02b78d93caa51ed166ff
Leandro Cadete <cadetedasilva@gmail.com>
2024-07-14 12:15:27 -0300

    Added textarea to send/receive
    1 - Send and receive message from a textarea

--- signalRTest_angular/src/app/app.component.ts
+++ signalRTest_angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppSignalrService } from './app-signalr.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,19 @@ export class AppComponent {
   title = 'signalR-test';
   receivedMessage: string = "";
   totalView: number = 0;
+  formMessage: FormGroup = new FormGroup({
+    msg: new FormControl('')
+  });
+
+
   constructor(private signalRService: AppSignalrService){}
+
 
   ngOnInit(): void {
     this.signalRService.startConnection().subscribe(() => {
       this.signalRService.receiveMessage().subscribe((message) => {
-        this.receivedMessage = this.title = message;
+        //this.receivedMessage = this.title = message;
+        this.formMessage.controls['msg'].setValue(message);
       });
 
       this.signalRService.updateTotalViews().subscribe((views) => this.totalView = views);
@@ -28,12 +36,13 @@ export class AppComponent {
 
 
   toggleLabel() {
-    if(this.title.charAt(0) == 's')
-      this.title = this.title.toUpperCase();
-    else 
-      this.title = this.title.toLowerCase();
+    const msg = this.formMessage.controls['msg'].value;
+    // if(this.title.charAt(0) == 's')
+    //   this.title = this.title.toUpperCase();
+    // else 
+    //   this.title = this.title.toLowerCase();
 
-    this.sendMessage(this.title);
+    this.sendMessage(msg);
     this.signalRService.newWindowLoadedOnClient();
   }
 }
